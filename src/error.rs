07c9687a1cfebabae64f19826cb5bee8ceb28f91
justin cremer <justin@trustpad.com>
2022-09-02 11:39:01 -0700

    adds condvar sigint handler for buffer flushing

---
+++ src/error.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    SerialPort(#[from] serialport::Error),
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,22 @@
 use serialport::SerialPort;
-use std::{
-    cell::RefCell,
-    rc::Rc,
-    sync::{Arc, Condvar, Mutex},
-    thread,
-    time::Duration,
-};
-use thiserror::Error;
+use std::{cell::RefCell, rc::Rc, thread, time::Duration};
 
-mod error;
-mod key_map;
+pub mod error;
+pub mod key_map;
 
-// use error::Result;
+use error::Result;
 use key_map::KeyCode;
 
 const FLUSH_BYTE: u8 = 0x38;
 
-fn main() -> Result<()> {
-    let port = Rc::new(RefCell::new(
+// #[test]
+fn open_browser() -> Result<()> {
+    let port = RefCell::new(
         serialport::new("COM3", 19_200)
             .timeout(Duration::from_millis(10))
             .open()?,
-    ));
+    );
 
-    // Flag for validating completion of tasks
-    let finished_pair = Arc::new((Mutex::new(false), Condvar::new()));
-
-    // Listen for SIGINT, setting the finish flag and notifying the condition variable upon
-    // receival
-    let finished_pair_clone = Arc::clone(&finished_pair);
-    let sig_handler = move || -> std::result::Result<(), ctrlc::Error> {
-        let (lock, cvar) = &*finished_pair_clone;
-        *lock.lock()? = true;
-        cvar.notify_one();
-        Ok(())
-    };
-    ctrlc::set_handler(sig_handler)?;
-
-    // if let Err(err) = self.start() {
-    //     self.shutdown()?;
-    //     return Err(err);
-    // }
-
-    if let Err(err) = run(Rc::clone(&port)) {
-        flush(Rc::clone(&port))?;
-        return Err(err)?;
-    }
-
-    // Wait for the thread to finish
-    let (lock, cvar) = &*finished_pair;
-    let mut started = lock.lock().unwrap();
-    while !*started {
-        started = cvar.wait(started).unwrap();
-    }
-
-    flush(port)
-}
-
-fn run(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
-    login(Rc::clone(&port))?;
-    thread::sleep(Duration::from_millis(10000));
-    write_document_gvim(Rc::clone(&port))?;
-    thread::sleep(Duration::from_millis(2000));
-
-
-    open_browser(port)
-}
-
-fn flush(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
-    port.borrow_mut().write(&vec![0x38])?;
-
-    Ok(())
-}
-
-fn open_browser(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
     port.borrow_mut()
         .write(&command![KeyCode::Super, KeyCode::R])?;
     thread::sleep(Duration::from_millis(1000));
@@ -86,7 +29,14 @@ fn open_browser(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
     Ok(())
 }
 
-fn write_document_notepad(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
+// #[test]
+fn write_document_notepad() -> Result<()> {
+    let port = RefCell::new(
+        serialport::new("COM3", 19_200)
+            .timeout(Duration::from_millis(10))
+            .open()?,
+    );
+
     let file_name = "temp.txt";
     let contents = std::fs::read_to_string(format!("./data/{file_name}"))?;
     let body = message!(&contents);
@@ -131,7 +81,14 @@ fn write_document_notepad(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> 
     Ok(())
 }
 
-fn login(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
+// #[test]
+fn login() -> Result<()> {
+    let port = RefCell::new(
+        serialport::new("COM3", 19_200)
+            .timeout(Duration::from_millis(10))
+            .open()?,
+    );
+
     port.borrow_mut().write(&command!(KeyCode::Space))?;
     thread::sleep(Duration::from_millis(1000));
 
@@ -140,7 +97,14 @@ fn login(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
     Ok(())
 }
 
-fn write_document_gvim(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
+#[test]
+fn write_document_gvim() -> Result<()> {
+    let port = RefCell::new(
+        serialport::new("COM3", 19_200)
+            .timeout(Duration::from_millis(10))
+            .open()?,
+    );
+
     // let file_name = "google-docs-manifest-generator.js";
     let file_name = "temp.txt";
     let contents = std::fs::read_to_string(format!("./data/{file_name}"))?;
@@ -196,18 +160,4 @@ fn write_document_gvim(port: Rc<RefCell<Box<dyn SerialPort>>>) -> Result<()> {
     thread::sleep(Duration::from_millis(1000));
 
     Ok(())
-}
-
-type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug, Error)]
-enum Error {
-    #[error(transparent)]
-    Lib(#[from] usb2usb::error::Error),
-    #[error(transparent)]
-    Ctrlc(#[from] ctrlc::Error),
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error(transparent)]
-    SerialPort(#[from] serialport::Error),
 }
